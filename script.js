09520246848161998d36088778b842894907fe3b
Sergey Strokatov <s.strokatov@webpractik.ru>
2020-08-14 00:10:42 +0300

    all done

---
+++ script.js
@@ -0,0 +1,180 @@
+var ServiceWorkers;
+(function (ServiceWorkers) {
+    ServiceWorkers.submitMood = 'create';
+    ServiceWorkers.shalowBook = {
+        author: '',
+        bookName: '0'
+    };
+    function setShalowBook(book) {
+        ServiceWorkers.shalowBook = book;
+    }
+    ServiceWorkers.setShalowBook = setShalowBook;
+    function setMoode(mode) {
+        ServiceWorkers.submitMood = mode;
+    }
+    ServiceWorkers.setMoode = setMoode;
+})(ServiceWorkers || (ServiceWorkers = {}));
+//установить новую библиотеку
+var setBooks = function (books) {
+    localStorage.setItem('books', JSON.stringify(books));
+};
+//получить текущую библиотеку
+var getBooks = function () { return JSON.parse(localStorage.getItem('books')); };
+//первичная инициализация библиотеки в localStorage
+var firstInitializeBooks = function () {
+    if (!getBooks()) {
+        changeTitle('There are not books, sir');
+        isListVisible(false);
+        setBooks([]);
+    }
+};
+//создать книгу
+var createBookItem = function (item, index) {
+    var li = document.createElement("LI");
+    var labelContainer = document.createElement('DIV');
+    var author = document.createElement('SPAN');
+    var bookName = document.createElement('SPAN');
+    var btnContainer = document.createElement('DIV');
+    var btnDelete = document.createElement('BUTTON');
+    var btnEdit = document.createElement('BUTTON');
+    labelContainer.className = "books__item-label-container";
+    li.className = "books__item-body";
+    author.className = "books__item-label";
+    bookName.className = "books__item-label";
+    btnContainer.className = "books__item-btn-container";
+    btnDelete.className = "books__btn books__item-btn-delete";
+    btnEdit.className = "books__btn books__item-btn-edit";
+    author.textContent = item.author;
+    bookName.textContent = item.bookName;
+    btnDelete.textContent = 'delete';
+    btnEdit.textContent = 'edit';
+    btnEdit.addEventListener('click', function () { return editBook(index); });
+    btnDelete.addEventListener('click', function () { return deleteBook(index); });
+    labelContainer.append(author, bookName, btnContainer);
+    btnContainer.append(btnEdit, btnDelete);
+    li.append(labelContainer, btnContainer);
+    return li;
+};
+//сделать blur на все книги, во время редактирвоания
+var addBlur = function () {
+    var liArr = document.querySelectorAll('.books__list > li');
+    liArr.forEach(function (item) {
+        item.classList.add('books__item-body--blur');
+    });
+};
+//удалить книгу
+var deleteBook = function (index) {
+    var books = getBooks();
+    books.splice(index, 1);
+    setBooks(books);
+    rendeListBooks();
+};
+//редактировать книгу
+var editBook = function (index) {
+    var books = getBooks();
+    var EditingBook = books[index];
+    ServiceWorkers.setShalowBook(EditingBook);
+    ServiceWorkers.setMoode('edit');
+    var author = document.querySelector('.books__form input[name="author"]');
+    var bookName = document.querySelector('.books__form input[name="bookName"]');
+    author.value = EditingBook.author;
+    bookName.value = EditingBook.bookName;
+    document.querySelector('.books__btn-add').classList.add("books__btn--disabled");
+    document.querySelector('.books__btn-edit').classList.remove("books__btn--disabled");
+    addBlur();
+};
+//рендер списка книг
+var rendeListBooks = function () {
+    var ul = document.querySelector('.books__list');
+    ul.innerHTML = '';
+    var list = getBooks();
+    list.forEach(function (item, index) {
+        ul.append(createBookItem(item, index));
+    });
+};
+//получение данных с формы при сабмите
+var getValuesFromForm = function (e) {
+    e.preventDefault();
+    var answer = {
+        author: '',
+        bookName: '0'
+    };
+    var form = document.querySelector('.books .books__form');
+    form.querySelectorAll("input").forEach(function (input) {
+        answer[input.name] = input.value || "";
+    });
+    form.reset();
+    return answer;
+};
+//обработка формы при создании книги
+var submitFormCreatingBook = function (e) {
+    var answer = getValuesFromForm(e);
+    var books = getBooks();
+    books.push(answer);
+    setBooks(books);
+    rendeListBooks();
+};
+//обработка формы при редактировании книги
+var submitFormEditingBook = function (e) {
+    ServiceWorkers.setMoode('create');
+    document.querySelector('.books__btn-add').classList.remove("books__btn--disabled");
+    document.querySelector('.books__btn-edit').classList.add("books__btn--disabled");
+    var answer = getValuesFromForm(e);
+    var books = getBooks();
+    // @ts-ignore
+    var index = books.findIndex(function (item) { return JSON.stringify(item) === JSON.stringify(ServiceWorkers.shalowBook); });
+    books[index] = answer;
+    setBooks(books);
+    rendeListBooks();
+};
+//обработка формы в зависимости от настройки
+var generalSubmitting = function (e) {
+    if (ServiceWorkers.submitMood === 'create') {
+        submitFormCreatingBook(e);
+    }
+    else {
+        submitFormEditingBook(e);
+    }
+};
+//поменять главный заголовок
+var changeTitle = function (text) {
+    document.querySelector('.books__title').textContent = text;
+};
+//показать-скрыть список
+var isListVisible = function (arg) {
+    var ul = document.querySelector('.books__list');
+    if (arg === true) {
+        ul.classList.remove('hidden');
+    }
+    else {
+        ul.classList.add('hidden');
+    }
+};
+//наблюдатель за элементами списка
+var myObserverForChild = function () {
+    if (getBooks().length !== 0) {
+        isListVisible(true);
+        changeTitle('Yours books, sir');
+    }
+    else {
+        changeTitle('There are not books, sir');
+        isListVisible(false);
+    }
+};
+// активировать наблюдатель за элементами списка
+var activateMyObserverForChild = function () {
+    var target = document.querySelector('.books__list');
+    var observer = new MutationObserver(function (mutations) {
+        myObserverForChild();
+    });
+    observer.observe(target, { childList: true });
+};
+//пеервичный жизненный цикл при загрузке страницы
+var firstLifecycle = function () {
+    firstInitializeBooks();
+    activateMyObserverForChild();
+    myObserverForChild();
+    rendeListBooks();
+    document.querySelector(".books__form").addEventListener('submit', generalSubmitting);
+};
+document.addEventListener("DOMContentLoaded", firstLifecycle);
---
+++ script.ts
@@ -0,0 +1,228 @@
+interface Book {
+    author: string,
+    bookName: string,
+}
+
+
+namespace ServiceWorkers {
+   export let submitMood:string = 'create';
+   export let shalowBook:Book = {
+    author: '',
+    bookName: '0'
+   }
+   export function setShalowBook(book:Book):void { //установить книгу, которая редактируется
+    shalowBook = book
+   }
+   export function setMoode(mode:string):void { //установить способ обработки формы
+    ServiceWorkers.submitMood = mode
+   }
+}
+
+//установить новую библиотеку
+const setBooks = (books:Array<Book>):void => {
+    localStorage.setItem('books', JSON.stringify(books))
+}
+
+//получить текущую библиотеку
+const getBooks = ():Array<Book> => JSON.parse(localStorage.getItem('books'))
+
+
+//первичная инициализация библиотеки в localStorage
+const firstInitializeBooks = () => {
+    if (!getBooks()) {
+        changeTitle('There are not books, sir')
+        isListVisible(false)
+        setBooks([])
+    } 
+}
+
+
+//создать книгу
+const createBookItem = (item:Book, index:any):HTMLElement => {
+    const li:HTMLElement = document.createElement("LI");
+
+    const labelContainer:HTMLElement = document.createElement('DIV');
+    const author:HTMLSpanElement = document.createElement('SPAN');
+    const bookName:HTMLSpanElement = document.createElement('SPAN');
+
+    const btnContainer:HTMLElement = document.createElement('DIV');
+    const btnDelete:HTMLElement = document.createElement('BUTTON');
+    const btnEdit:HTMLElement = document.createElement('BUTTON');
+
+    labelContainer.className = "books__item-label-container"
+    li.className = "books__item-body";
+    author.className = "books__item-label";
+    bookName.className = "books__item-label";
+
+    btnContainer.className = "books__item-btn-container"
+    btnDelete.className = "books__btn books__item-btn-delete";
+    btnEdit.className = "books__btn books__item-btn-edit";
+
+    author.textContent = item.author;
+    bookName.textContent = item.bookName;
+
+    btnDelete.textContent = 'delete';
+    btnEdit.textContent = 'edit';
+
+    btnEdit.addEventListener('click', () => editBook(index))
+    btnDelete.addEventListener('click', () => deleteBook(index))
+
+    labelContainer.append(author, bookName, btnContainer)
+    btnContainer.append(btnEdit, btnDelete)
+
+    li.append(labelContainer, btnContainer);
+
+    return li
+}
+
+//сделать blur на все книги, во время редактирвоания
+const addBlur = (): void => {
+    const liArr = document.querySelectorAll('.books__list > li')
+    liArr.forEach((item) => {
+            item.classList.add('books__item-body--blur')
+    })
+}
+
+//удалить книгу
+const deleteBook = (index:number): void => {
+    const books:Array<Book> = getBooks()
+    books.splice(index, 1)
+    setBooks(books)
+    rendeListBooks()
+}
+
+//редактировать книгу
+const editBook = (index:number) => {
+    const books:Book[] = getBooks();
+    const EditingBook:Book = books[index];
+
+    ServiceWorkers.setShalowBook(EditingBook);
+    ServiceWorkers.setMoode('edit')
+
+    let author: HTMLInputElement = document.querySelector('.books__form input[name="author"]');
+    let bookName: HTMLInputElement = document.querySelector('.books__form input[name="bookName"]');
+
+    author.value = EditingBook.author;
+    bookName.value = EditingBook.bookName;
+
+    document.querySelector('.books__btn-add').classList.add("books__btn--disabled");
+    document.querySelector('.books__btn-edit').classList.remove("books__btn--disabled");
+
+    addBlur()
+}
+
+//рендер списка книг
+const rendeListBooks = (): void => {
+    const ul = document.querySelector('.books__list');
+    ul.innerHTML = '';
+    const list: Book[] = getBooks();
+    list.forEach((item, index) => {
+        ul.append(createBookItem(item, index));
+    })
+}
+
+//получение данных с формы при сабмите
+const getValuesFromForm = (e:Event) => {
+    e.preventDefault();
+    let answer:Book | any  = {
+        author: '',
+        bookName: '0'
+       };
+    const form: HTMLFormElement = document.querySelector('.books .books__form');
+    form.querySelectorAll("input").forEach(input => {
+        answer[input.name] = input.value || ""
+    });
+    form.reset();
+    return answer
+}
+
+
+//обработка формы при создании книги
+const submitFormCreatingBook = (e:Event): void => {
+
+    let answer = getValuesFromForm(e)
+    let books = getBooks();
+
+    books.push(answer);
+    setBooks(books)
+    rendeListBooks();
+}
+
+//обработка формы при редактировании книги
+const submitFormEditingBook = (e:Event): void => {
+    ServiceWorkers.setMoode('create');
+
+    document.querySelector('.books__btn-add').classList.remove("books__btn--disabled");
+    document.querySelector('.books__btn-edit').classList.add("books__btn--disabled");
+
+    let answer = getValuesFromForm(e)
+    let books = getBooks();
+    // @ts-ignore
+    const index = books.findIndex((item:Book) => JSON.stringify(item) === JSON.stringify(ServiceWorkers.shalowBook)); 
+    books[index] = answer;
+
+    setBooks(books)
+    rendeListBooks();
+}
+
+//обработка формы в зависимости от настройки
+const generalSubmitting = (e:Event) => {
+    if (ServiceWorkers.submitMood === 'create') {
+        submitFormCreatingBook(e);
+    } else {
+        submitFormEditingBook(e);
+    }
+}
+
+//поменять главный заголовок
+const changeTitle = (text:string) => {
+    document.querySelector('.books__title').textContent = text;
+}
+
+//показать-скрыть список
+const isListVisible = (arg:boolean):void => {
+    const ul:HTMLElement = document.querySelector('.books__list');
+    if (arg === true) {
+        ul.classList.remove('hidden');
+    } else {
+        ul.classList.add('hidden')
+    }
+}
+
+//наблюдатель за элементами списка
+const myObserverForChild = (): void => {
+    if (getBooks().length !== 0) {
+        isListVisible(true)
+        changeTitle('Yours books, sir')
+    } else {
+        changeTitle('There are not books, sir')
+        isListVisible(false)
+    }   
+}
+
+
+// активировать наблюдатель за элементами списка
+const activateMyObserverForChild = () => {
+    let target:HTMLElement = document.querySelector('.books__list');
+    let observer = new MutationObserver(mutations => {
+        myObserverForChild()
+    })
+    observer.observe(target, { childList:true })
+}
+
+
+//пеервичный жизненный цикл при загрузке страницы
+const firstLifecycle = () => {
+    firstInitializeBooks()
+    activateMyObserverForChild()
+    myObserverForChild()
+    rendeListBooks();
+    document.querySelector(".books__form").addEventListener('submit', generalSubmitting);
+}
+
+
+document.addEventListener("DOMContentLoaded", firstLifecycle);
+
+
+
+
